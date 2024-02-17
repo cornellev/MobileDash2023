ceb475a45a91bf17eaf63e3cdead24429519065a
Daniel Sorokin <danielsapiary@gmail.com>
2024-02-17 11:37:09 -0500

    epic

--- components/SpeedWidget.jsx
+++ components/SpeedWidget.jsx
@@ -15,7 +15,7 @@ const startColor = [150, 0, 50]; // Red
 const endColor = [0, 150, 50]; // Green
 
 const maxSpeed = 20;
-const speed = 20; // Need webhook to get this value
+const speed = 20; // Need to get this value
 const speedBarWidth = `${(Math.max(0, Math.min(speed, maxSpeed)) / maxSpeed) * 98}%`; // Need to be updating this value on speed value change
 const speedBarColor = interpolateColor(speed, 0, 20, startColor, endColor);
 
